01d21339182b780051a05400a3fc4ecb9a060a96
Oscar Munoz <omunozg@emory.edu>
2019-03-11 03:20:12 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+def instantiate_new_array
+  y = []
+  return y
+end
+
+
+def instantiate_new_array
+  ary = Array.new
+end
+
+def array_with_two_elements
+  x = Array.new(2)
+  return x
+end
+
+def first_element(array)
+  array.at(0)
+end
+
+def third_element(array)
+  array.at(2)
+end
+
+def last_element(array)
+  array.last
+end
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length 
+end
+
+  
